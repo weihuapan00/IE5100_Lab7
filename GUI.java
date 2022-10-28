06f916f347197ecb94e0a0536cdf773697bbd5a8
weihuapan00 <w5pan@ucsd.edu>
2022-10-28 12:47:29 -0700

    finish

--- GUI.java
+++ GUI.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.LinkedList;
+
 import javax.swing.*;
 
 public class GUI extends JFrame{
+    private fileOpener f;
     GUI(){
         // set up the frame default constraint 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,14 +14,46 @@ public class GUI extends JFrame{
 
         this.add(new JLabel("Reading FIle Name"));
         this.add(new JLabel("File name to write"));
-        this.add(new JLabel("Annual.csv"));
-        this.add(new JTextField("<user input>"));
-        this.add(new Button("Click to read from file"));
-        this.add(new Button("Click to write to file"));
+
+        JTextField tf1 = new JTextField("annual.csv");
+        this.add(tf1);
+        JTextField tf2 = new JTextField("<user input>");
+        this.add(tf2);
+        JButton b1 = new JButton("Click to read from file");
+        JButton b2 = new JButton("Click to write to file");
+        this.add(b1);
+        this.add(b2);
+
         this.add(new JLabel("First five lines of file"));
         this.add(new JLabel("First five lines of new file"));
-        this.add(new JTextArea());
-        this.add(new JTextArea());
+
+        var ta1 = new JTextArea();
+        this.add(ta1);
+        var ta2 = new JTextArea();
+        this.add(ta2);
+
+        b1.addActionListener(e -> {
+            ta1.setText("");
+            String filename = tf1.getText();
+            f = new fileOpener(filename);
+            f.readFile();
+            LinkedList<String> list = f.getStr_list();
+            for (int i=0; i<5; i++){
+                ta1.append(list.get(i)+ "\n");
+            }
+        });
+
+        b2.addActionListener(e -> {
+            ta2.setText("");
+            String filename = tf2.getText();
+            f.writeFile(filename);
+            f = new fileOpener(filename);
+            f.readFile();
+            LinkedList<String> list = f.getStr_list(); 
+            for (int i=0; i<5; i++){
+                ta2.append(list.get(i)+ "\n");
+            }
+        });
 
         
 
@@ -29,5 +64,6 @@ public class GUI extends JFrame{
 
     public static void main(String[] args){
         new GUI();
+    
     }
 }
--- fileOpener.java
+++ fileOpener.java
@@ -4,15 +4,16 @@ public class fileOpener {
     private String filename;// filename location
     private LinkedList<String> str_list = new LinkedList<>();
     
+    
     //constructor
     public fileOpener(String filename){
-       setFilename(filename); 
+       setFilename(filename);
     }
 
     // read file from filename
     public void readFile(){
         try {
-            File myObj = new File("Annual.csv");
+            File myObj = new File(filename);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
               String data = myReader.nextLine();
@@ -31,7 +32,14 @@ public class fileOpener {
         try {
             FileWriter myWriter = new FileWriter(filename);
             for (String str : str_list){
-                myWriter.write(str);
+                String[] list = str.split(",",4);
+                String toWrite = "";
+                for(int i = 0; i<2; i++){
+                  toWrite += list[i] + ",";
+                }
+                toWrite += list[2] + "\n";
+
+                myWriter.write(toWrite);
             }
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
@@ -48,7 +56,10 @@ public class fileOpener {
         this.filename = filename;
     }
 
-    
+    public LinkedList<String> getStr_list() {
+        return str_list;
+    }
+
 
 }
 
